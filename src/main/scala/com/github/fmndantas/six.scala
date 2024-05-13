08b1def29c697a64af759d942cc9830fc279660e
Fernando <fernandodantas1996@gmail.com>
2024-05-13 12:38:16 -0300

    feat: Continues chapter 6

--- src/main/scala/com/github/fmndantas/six.scala
+++ src/main/scala/com/github/fmndantas/six.scala
@@ -1,6 +1,6 @@
 package com.github.fmndantas
 
-package six {
+trait Six {
   trait RNG {
     def nextInt: (Int, RNG)
   }
@@ -15,30 +15,72 @@ package six {
   }
 
   def nonNegativeInt(rng: RNG): (Int, RNG) = {
-    val (n, nextRng) = rng.nextInt
-    (if (n < 0) -(n + 1) else n, nextRng)
+    val (a, rngA) = rng.nextInt
+    (if (a < 0) -(a + 1) else a, rngA)
   }
 
   def double: Rand[Double] = map(nonNegativeInt)(_.toDouble / Int.MaxValue)
 
+  // NOTE: S => (A, S)
   type Rand[+A] = RNG => (A, RNG)
 
   def unit[A](a: A): Rand[A] = rng => (a, rng)
 
-  def map[A, B](s: Rand[A])(f: A => B): Rand[B] = rng => {
-    val (nextN, nextRng) = s(rng)
-    (f(nextN), nextRng)
-  }
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] = flatMap(s)(f.andThen(unit))
 
   def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
-    rng => {
-      val (a, rngA) = ra(rng)
-      val (b, rngB) = rb(rng)
-      (f(a, b), rngB)
+    flatMap(ra) { a =>
+      flatMap(rb) { b =>
+        unit(f(a, b))
+      }
     }
 
   def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
     fs.foldLeft((x: RNG) => (List.empty[A], x)) { case (acc, rand) =>
       map2(acc, rand) { case (a, b) => a :+ b }
     }
+
+  def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] =
+    rng => {
+      val (a, rngA) = f(rng)
+      g(a)(rngA)
+    }
+
+  def nonNegativeLessThan(n: Int): Rand[Int] = flatMap(nonNegativeInt) { i =>
+    val mod = i % n
+    if (i + n - 1 - mod >= 0) unit(mod)
+    else nonNegativeLessThan(n)
+  }
+
+  /*
+   * Encapsula transições de estado de
+   * maneira funcional; faz isso retornando
+   * um novo estado atualizado que resultou
+   * da atualização de um estado passado
+   */
+  case class State[S, +A](run: S => (A, S)):
+    def map[B](f: A => B): State[S, B] = flatMap(f andThen State.unit)
+
+    def flatMap[B](f: A => State[S, B]): State[S, B] =
+      State { s0 =>
+        val (a, s1) = run(s0)
+        f(a).run(s1)
+      }
+
+  object State:
+    def unit[A, S](a: A) = State[S, A](s => (a, s))
+
+    def map2[A, B, C, S](sa: State[S, A], sb: State[S, B])(
+        f: (A, B) => C
+    ): State[S, C] = for {
+      a <- sa
+      b <- sb
+    } yield (f(a, b))
+
+    def sequence[A, S](ss: List[State[S, A]]): State[S, List[A]] =
+      ss.foldLeft(State.unit[List[A], S](List.empty[A])) { case (acc, s) =>
+        acc.flatMap(l => s.map(a => l :+ a))
+      }
+
+  def getNextInt: State[RNG, Int] = State(_.nextInt)
 }
--- src/test/scala/sixSpec.scala
+++ src/test/scala/sixSpec.scala
@@ -1,26 +1,93 @@
 import org.scalatest.funspec.AnyFunSpec
 
-import com.github.fmndantas.six._
+import com.github.fmndantas.Six
 
-class SixSpec extends AnyFunSpec {
-  def mockRand[A](desiredValue: A)(rng: RNG) = (desiredValue, rng)
+class sutSpec extends AnyFunSpec {
+  object sut extends Six
+  import sut.{State, SimpleRNG, RNG}
 
-  it("map2 joins two rands") {
-    val rng = SimpleRNG(42)
-    val (result1, _) = nonNegativeInt(rng)
-    val (result2, _) = double(rng)
-    val (joinedResult, _) = map2(nonNegativeInt, double)(_ + _)(rng)
-    assertResult(result1 + result2)(joinedResult)
+  case class SomeState(x: Int)
+
+  def mockRand[A](desiredValue: A)(rng: sut.RNG) = (desiredValue, rng)
+
+  describe("Non-generalized functions") {
+    it("map2 joins two rands") {
+      val rng = SimpleRNG(42)
+      val (result1, _) = mockRand(42)(rng)
+      val (result2, _) = mockRand(42)(rng)
+      val (joinedResult, _) = sut.map2(mockRand(42), mockRand(42))(_ + _)(rng)
+      assertResult(result1 + result2)(joinedResult)
+    }
+
+    it("sequence transforms list of transitions into a single transition") {
+      val rands = List(mockRand(1), mockRand(2), mockRand(3))
+      val transition = sut.sequence(rands)
+      val rng = SimpleRNG(42)
+      val (resultingList, resultingRng) = transition(rng)
+      assertResult(List(1, 2, 3))(resultingList)
+      rands.lastOption.foreach { lastRand =>
+        assertResult(lastRand(rng)._2)(resultingRng)
+      }
+    }
+
+    it("nonNegativeLessThan returns aleatory number below specific n") {
+      val rng = SimpleRNG(42)
+      val (result, _) = sut.nonNegativeLessThan(10)(rng)
+      assert(result < 10)
+    }
   }
 
-  it("sequence transforms list of transitions into a single transition") {
-    val rands = List(mockRand(1), mockRand(2), mockRand(3))
-    val transition = sequence(rands)
-    val rng = SimpleRNG(42)
-    val (resultingList, resultingRng) = transition(rng)
-    assertResult(List(1, 2, 3))(resultingList)
-    rands.lastOption.foreach { lastRand =>
-      assertResult(lastRand(rng)._2)(resultingRng)
+  describe("State") {
+    it("Lifts some value") {
+      val initialState = SomeState(-1)
+      val s1 = State.unit[Int, SomeState](10)
+      val (a, b) = s1.run(initialState)
+      assertResult(10)(a)
+      assertResult(b)(initialState)
     }
+
+    it("Maps one state into another") {
+      val initialState = SomeState(0)
+      val s1 = State[SomeState, Int](s => (0, s.copy(x = s.x + 1)))
+      val s2 = s1.map(_ + 10)
+      val (r, s3) = s2.run(initialState)
+      assertResult(10)(r)
+      assertResult(SomeState(1))(s3)
+    }
+
+    it("Flatmaps one state into another") {
+      val initialState = SomeState(0)
+      val s1 = State[SomeState, Int](s => (0, s.copy(x = s.x + 1)))
+      val s2 = s1.flatMap(a =>
+        State { s =>
+          (a + 10, s.copy(x = s.x + 1))
+        }
+      )
+      val (r, s3) = s2.run(initialState)
+      assertResult(10)(r)
+      assertResult(SomeState(2))(s3)
+    }
+
+    it("Transform list of states into one state that generate list as result") {
+      val s = State.sequence[Int, RNG](
+        List(State.unit(42), State.unit(43), State.unit(44))
+      )
+      val (r, _) = s.run(SimpleRNG(42))
+      assertResult(List(42, 43, 44))(r)
+    }
+  }
+
+  it("How to use State to generate random integers") {
+    val simpleRNG = SimpleRNG(100)
+    val s = for {
+      a <- State.unit[Int, RNG](0)
+      b <- sut.getNextInt
+      c <- sut.getNextInt
+    } yield (a, b, c)
+    val ((a, b, c), _) = s.run(simpleRNG)
+    // println((a, b, c))
+    assert(a != b)
+    assert(b != c)
+    assert(c != a)
   }
 }
