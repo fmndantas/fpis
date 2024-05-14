f45db13fe2b8798e9bb5df982ed13bbfadd04546
Fernando <fernandodantas1996@gmail.com>
2024-05-14 08:10:38 -0300

    refactor: Renames some classes

--- src/main/scala/com/github/fmndantas/Six.scala
+++ src/main/scala/com/github/fmndantas/Six.scala
@@ -52,12 +52,11 @@ trait Six {
     else nonNegativeLessThan(n)
   }
 
-  /*
-   * Encapsula transições de estado de
-   * maneira funcional; faz isso retornando
-   * um novo estado atualizado que resultou
-   * da atualização de um estado passado
-   */
+  // NOTE:
+  // Encapsula transições de estado de
+  // maneira funcional; faz isso retornando
+  // um novo estado atualizado que resultou
+  // da atualização de um estado passado
   case class State[S, +A](run: S => (A, S)):
     def map[B](f: A => B): State[S, B] = flatMap(f andThen State.unit)
 
