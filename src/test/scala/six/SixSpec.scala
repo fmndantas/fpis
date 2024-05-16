02da64c3d58b0cd7dc44d3c8dae6a48d818478d4
Fernando <fernandodantas1996@gmail.com>
2024-05-16 17:26:59 -0300

    refactor: SixSpec.scala, Scalatest -> munit

--- src/test/scala/six/SixSpec.scala
+++ src/test/scala/six/SixSpec.scala
@@ -2,41 +2,38 @@ import com.github.fmndantas.six.RNG
 import com.github.fmndantas.six.SimpleRNG
 import com.github.fmndantas.six.Six
 import com.github.fmndantas.six.State
-import org.scalatest.funspec.AnyFunSpec
 
-class SixSpec extends AnyFunSpec {
+class SixSpec extends munit.FunSuite {
   object sut extends Six
 
   def mockRand[A](desiredValue: A)(rng: RNG) = (desiredValue, rng)
 
-  describe("Non-generalized functions") {
-    it("map2 joins two rands") {
-      val rng = SimpleRNG(42)
-      val (result1, _) = mockRand(42)(rng)
-      val (result2, _) = mockRand(42)(rng)
-      val (joinedResult, _) = sut.map2(mockRand(42), mockRand(42))(_ + _)(rng)
-      assertResult(result1 + result2)(joinedResult)
-    }
+  test("map2 joins two rands") {
+    val rng = SimpleRNG(42)
+    val (result1, _) = mockRand(42)(rng)
+    val (result2, _) = mockRand(42)(rng)
+    val (joinedResult, _) = sut.map2(mockRand(42), mockRand(42))(_ + _)(rng)
+    assertEquals(joinedResult, result1 + result2)
+  }
 
-    it("sequence transforms list of transitions into a single transition") {
-      val rands = List(mockRand(1), mockRand(2), mockRand(3))
-      val transition = sut.sequence(rands)
-      val rng = SimpleRNG(42)
-      val (resultingList, resultingRng) = transition(rng)
-      assertResult(List(1, 2, 3))(resultingList)
-      rands.lastOption.foreach { lastRand =>
-        assertResult(lastRand(rng)._2)(resultingRng)
-      }
-    }
-
-    it("nonNegativeLessThan returns aleatory number below specific n") {
-      val rng = SimpleRNG(42)
-      val (result, _) = sut.nonNegativeLessThan(10)(rng)
-      assert(result < 10)
+  test("sequence transforms list of transitions into a single transition") {
+    val rands = List(mockRand(1), mockRand(2), mockRand(3))
+    val transition = sut.sequence(rands)
+    val rng = SimpleRNG(42)
+    val (resultingList, resultingRng) = transition(rng)
+    assertEquals(resultingList, List(1, 2, 3))
+    rands.lastOption.foreach { lastRand =>
+      assertEquals(resultingRng, lastRand(rng)._2)
     }
   }
 
-  it("How to use State to generate random integers") {
+  test("nonNegativeLessThan returns aleatory number below specific n") {
+    val rng = SimpleRNG(42)
+    val (result, _) = sut.nonNegativeLessThan(10)(rng)
+    assert(result < 10)
+  }
+
+  test("How to use State to generate random integers") {
     val simpleRNG = SimpleRNG(100)
     val s = for {
       a <- State.unit[Int, RNG](0)
