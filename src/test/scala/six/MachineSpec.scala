c5922df76dde672853dccc649e9b2bf47193a667
Fernando <fernandodantas1996@gmail.com>
2024-05-16 17:46:50 -0300

    refactor: MachineSpec.scala, Scalatest -> munit

--- src/test/scala/six/MachineSpec.scala
+++ src/test/scala/six/MachineSpec.scala
@@ -1,57 +1,53 @@
 import com.github.fmndantas.six.candyDispenser.Machine
-import org.scalatest.funspec.AnyFunSpec
 
-class MachineSpec extends AnyFunSpec {
-  describe("Inserting a coin") {
-    describe("Into a locked machine") {
-      it("Will unlock it if there is some candy left") {
-        val m0 = Machine(locked = true, candies = 10, coins = 10)
-        val m1 = m0.insertCoin
-        assertResult(Machine(false, 10, 11))(m1)
-      }
-    }
-    describe("Into a unlocked machine does nothing") {
-      it("On a machine that has some candies") {
-        val m0 = Machine(locked = false, candies = 10, coins = 10)
-        val m1 = m0.insertCoin
-        assertResult(m0)(m1)
-      }
-
-      it("On a machine that's out of candies") {
-        val m0 = Machine(locked = false, candies = 0, coins = 10)
-        val m1 = m0.insertCoin
-        assertResult(m0)(m1)
-      }
-    }
+class MachineSpec extends munit.FunSuite {
+  test(
+    "Inserting a coin into a locked machine will unlock it if there is some candy left"
+  ) {
+    val m0 = Machine(locked = true, candies = 10, coins = 10)
+    val m1 = m0.insertCoin
+    assertEquals(m1, Machine(false, 10, 11))
   }
 
-  describe("Turning the knob") {
-    it("Will do nothing on a locked machine") {
-      val m0 = Machine(locked = true, candies = 10, coins = 10)
-      val m1 = m0.turnKnob
-      assertResult(m0)(m1)
-    }
-
-    it(
-      "On a unlocked machine will cause machine to dispense candy and become locked"
-    ) {
-      val m0 = Machine(locked = false, candies = 10, coins = 10)
-      val m1 = m0.turnKnob
-      assertResult(Machine(true, 9, 10))(m1)
-    }
+  test(
+    "Inserting a coin into a unlocked machine does nothing on a machine that has some candies"
+  ) {
+    val m0 = Machine(locked = false, candies = 10, coins = 10)
+    val m1 = m0.insertCoin
+    assertEquals(m1, m0)
   }
 
-  describe("A machine that's out of candies ignores") {
-    it("Any coin insertion") {
+  test(
+    "Inserting a coin into a unlocked machine does nothing on a machine that's out of candies"
+  ) {
+    val m0 = Machine(locked = false, candies = 0, coins = 10)
+    val m1 = m0.insertCoin
+    assertEquals(m1, m0)
+  }
+
+  test("Turning the knob will do nothing on a locked machine") {
+    val m0 = Machine(locked = true, candies = 10, coins = 10)
+    val m1 = m0.turnKnob
+    assertEquals(m1, m0)
+  }
+
+  test(
+    "Turning the knob on a unlocked machine will cause machine to dispense candy and become locked"
+  ) {
+    val m0 = Machine(locked = false, candies = 10, coins = 10)
+    val m1 = m0.turnKnob
+    assertEquals(m1, Machine(true, 9, 10))
+  }
+
+  test("A machine that's out of candies ignores any coin insertion") {
       val m0 = Machine(locked = true, candies = 0, coins = 10)
       val m1 = m0.insertCoin
-      assertResult(m0)(m1)
-    }
+      assertEquals(m1, m0)
+  }
 
-    it("Any knob turning") {
+  test("A machine that's out of candies ignores any knob turning") {
       val m0 = Machine(locked = false, candies = 0, coins = 10)
       val m1 = m0.turnKnob
-      assertResult(m0)(m1)
-    }
+      assertEquals(m1, m0)
   }
 }
