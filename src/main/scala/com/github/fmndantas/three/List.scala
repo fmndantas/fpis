aeffe6a2f64ff38f145eb64b33ce36935d5a1b5b
Fernando <fernandodantas1996@gmail.com>
2024-05-24 10:32:36 -0300

    feat: 3.15

--- src/main/scala/com/github/fmndantas/three/List.scala
+++ src/main/scala/com/github/fmndantas/three/List.scala
@@ -17,6 +17,8 @@ sealed trait List[+A] {
 
   // NOTE: based on foldRight
   def append[A2 >: A](v: A2): List[A2] = foldRight(List(v))(List.cons)
+
+  def appendAll[A2 >: A](v: List[A2]): List[A2] = foldRight(v)(List.cons)
 }
 case object Nil extends List[Nothing]
 case class Cons[+A](h: A, t: List[A]) extends List[A]
@@ -29,4 +31,7 @@ object List {
   def empty[A]: List[A] = Nil
 
   def cons[A](h: A, t: List[A]): List[A] = Cons(h, t)
+
+  def concatenate[A](v: List[List[A]]): List[A] =
+    v.foldLeft(List.empty[A])((b, a) => b.appendAll(a))
 }
--- src/test/scala/three/ListSpec.scala
+++ src/test/scala/three/ListSpec.scala
@@ -37,4 +37,12 @@ class ListSpec extends MultipleCases {
   test("append") {
     assertEquals(List(1, 2, 3).append(4), List(1, 2, 3, 4))
   }
+
+  test("appendAll") {
+    assertEquals(List(1, 2, 3).appendAll(List(4, 5, 6)), List(1, 2, 3, 4, 5, 6))
+  }
+
+  test("concatenate") {
+    assertEquals(List.concatenate(List(List(1, 2, 3), List(4, 5, 6), List(7, 8, 9))), List(1, 2, 3, 4, 5, 6, 7, 8, 9))
+  }
 }
