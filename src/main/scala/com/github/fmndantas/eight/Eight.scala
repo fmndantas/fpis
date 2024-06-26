29898ac22472740606e112495068ae821fa9f88b
Fernando <fernandodantas1996@gmail.com>
2024-06-26 15:34:43 -0300

    feat: Exemplo de uso da lib do capítulo 8

--- src/main/scala/com/github/fmndantas/eight/Eight.scala
+++ src/main/scala/com/github/fmndantas/eight/Eight.scala
@@ -1,9 +1,12 @@
 package com.github.fmndantas.eight
 
+import com.github.fmndantas.six.SimpleRNG
+
 object Eight extends App:
-  println("Ok")
-  // TODO: fix example
-  // val intList = Gen.listOf(Gen.choose(0, 100))
-  // val prop =
-  //   Prop.forAll(intList)(ns => ns.reverse.reverse == ns) &&
-  //     Prop.forAll(intList)(ns => ns.reverse.headOption == ns.lastOption)
+  // FIX: incomplete chapter
+  val intList = Gen.choose(0, 100).listOfN(Gen.choose(0, 100))
+  val prop =
+    Prop.forAll(intList)(ns => ns.reverse.reverse == ns) &&
+      Prop.forAll(intList)(ns => ns.reverse.headOption == ns.lastOption)
+  val r = prop.run(100, SimpleRNG(System.currentTimeMillis))
+  println(r)
